1abc49f52066578df0db3220e082ffbac264b9c5
GGrop <giuliano.gropponi@gmail.com>
2023-04-08 13:10:44 -0300

    declares multer
    multer is a middleware designed to handle multipart/form-data in forms. that is our way to upload image files

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const PORT = '8007';
 
 const fs = require('fs');
 const exphb = require('express-handlebars').engine;
+
+
+const multer = require('multer');
+
+const upload = multer({ dest: './uploads/shields' });
 app.engine('handlebars', exphb());
 app.set('view engine', 'handlebars');
