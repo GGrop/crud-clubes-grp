8826e42b27e8a592763d71966ffb0b7145e9233d
GGrop <giuliano.gropponi@gmail.com>
2023-04-08 13:08:34 -0300

    declares exphb
    require express-handlebars with .engine to inicializate handlebars in app

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const app = express();
 const PORT = '8007';
 
 const fs = require('fs');
+const exphb = require('express-handlebars').engine;
+app.engine('handlebars', exphb());
+app.set('view engine', 'handlebars');
